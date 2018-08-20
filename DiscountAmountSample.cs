5a7f1e8f66820f98c4587860abd16e1356ec4a81
KrzysztofPajak <support@grandnode.com>
2018-08-20 08:15:36 +0200

    Init

---
+++ DiscountAmountSample.cs
@@ -0,0 +1,35 @@
+﻿using Grand.Core.Plugins;
+using Grand.Services.Discounts;
+using Grand.Core.Domain.Discounts;
+using Grand.Core;
+
+namespace Grand.Plugin.DiscountAmount.Sample
+{
+    public class DiscountAmountSample : BasePlugin, IDiscountAmountProvider
+    {
+        private readonly IWebHelper _webHelper;
+        public DiscountAmountSample(IWebHelper webHelper)
+        {
+            this._webHelper = webHelper;
+        }
+
+        public decimal DiscountAmount(Discount discount, decimal amount)
+        {
+            return 1;
+        }
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/DiscountAmountSample/Configure";
+        }
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            //locales
+            base.Uninstall();
+        }
+    }
+}
